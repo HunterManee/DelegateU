0b52587466cc0dedb4617aa6e9a44a4c2cc737e2
3A33 <huntermanee3a33@outlook.com>
2024-07-26 02:03:14 -0400

    Restructe app.js

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,26 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const bcrypt = require('bcrypt');
+const ConnectionManager = require('./ConnectionManager');
+const schemaGroupLogin = require('./models/GroupLogin');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-//Connection Manager
-const bcrypt = require('bcrypt');
-const ConnectionManager = require('./ConnectionManager');
-const schemaGroupLogin = require('./models/GroupLogin');
-
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 //Import Routes
 const peopleRouter = require('./routes/people');
-app.use('/people', peopleRouter);
-
 const positionsRouter = require('./routes/positions');
-app.use('/positions', positionsRouter);
-
 const roleRouter = require('./routes/roles');
-app.use('/roles', roleRouter);
-
 const breakRoute = require('./routes/breaks');
+app.use('/people', peopleRouter);
+app.use('/positions', positionsRouter);
+app.use('/roles', roleRouter);
 app.use('/breaks', breakRoute);
 
 
