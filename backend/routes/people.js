1a88e847a0318b99c371c5e2fea251d111e7aa71
3A33 <huntermanee3a33@outlook.com>
2024-08-05 12:53:00 -0400

    Forgot to update axios commands in people

--- backend/routes/people.js
+++ backend/routes/people.js
@@ -156,10 +156,10 @@ router.patch('/:personId', async(req,res)=>{
             };
             if(req.body.break === 'start') {
                 data['breakLength'] = req.body.breakLength;
-                await axios.post(`http://localhost:3000/breaks?groupId=${groupId}`,data);
+                await axios.post(`https://delegateubackend.azurewebsites.net?groupId=${groupId}`,data);
             }
             else if(req.body.break === 'incomplete') {
-                await axios.delete(`http://localhost:3000/breaks/${data.personId}?groupId=${groupId}`)
+                await axios.delete(`https://delegateubackend.azurewebsites.net/${data.personId}?groupId=${groupId}`)
             }
             
         }
